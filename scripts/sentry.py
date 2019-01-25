eaa79066cad717660d884e5e9a70b0802bf5574b
jlat96 <latinojr@student.cs.jmu.edu>
2019-01-25 15:48:27 -0500

    finished lab

--- scripts/sentry.py
+++ scripts/sentry.py
@@ -6,11 +6,12 @@ SentryBot lets us know if an intruder walks past.
 Author: 
 Version:
 """
-
+import numpy as np
 import rospy
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from kobuki_msgs.msg import Sound
 import numpy as np
 
 
@@ -32,17 +33,55 @@ class SentryNode(object):
         self.cv_bridge = CvBridge()
         rospy.Subscriber('/camera/depth_registered/image',
                          Image, self.depth_callback, queue_size=1)
-        rospy.spin()
+        self.sound_publish = rospy.Publisher('/mobile_base/commands/sound', Sound, queue_size=1)
+        self.prev = None
+        self.average = 0
+        self.threshold = 15  
+        self.intruder = False  
+
+
+        #rospy.spin()
+        
 
     def depth_callback(self, depth_msg):
         """ Handle depth callbacks. """
 
         # Convert the depth message to a numpy array
         depth = self.cv_bridge.imgmsg_to_cv2(depth_msg)
+        
+        current = depth[:, depth.shape[1] / 2]
+        #current = current[~np.isnan(current)]
 
-        # YOUR CODE HERE.
-        # HELPER METHODS ARE GOOD.
-
+        
+        if self.prev is None:
+            self.prev = current
+        
+        
+        d = np.abs(current - self.prev)
+        
+        d = d[~np.isnan(d)]
+        d = np.sum(d)
+        
+        
+        
+        self.average = self.average * .5 + d * (1 - .5)
+        
+        self.prev = current
+        if(self.average > self.threshold):
+            
+            self.intruder = True
+        
+    def run(self):
+        
+        while(True and not rospy.is_shutdown()):
+            beep = Sound()
+            
+            if(self.intruder):
+                print self.average
+                beep.value = 0
+                self.sound_publish.publish(beep)
+                self.intruder = False
 
 if __name__ == "__main__":
-    SentryNode()
+    x = SentryNode()
+    x.run()
